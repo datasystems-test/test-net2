021f5ce57f11101e95114cdd24254577bb08091c
Brayan Castillo <brayan.castillo@datasystems.com.mx>
2021-04-23 17:18:19 -0500

    commit inicial

---
+++ CRUD-NETCore-TDD.Infra/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_NETCore_TDD.Infra.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public User(int Id, string Name, int Age, bool IsActive)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+            this.IsActive = IsActive;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ CRUD-NETCore-TDD.Test/Tests/BaseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CRUD_NETCore_TDD.Infra.Repositories;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CRUD_NETCore_TDD.Test.Tests
+{
+    public class BaseTest
+    {
+        protected MyContext ctx;
+        public BaseTest(MyContext ctx = null)
+        {
+            this.ctx = ctx ?? GetInMemoryDBContext();
+        }
+        protected MyContext GetInMemoryDBContext()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<MyContext>();
+            var options = builder.UseInMemoryDatabase("test").UseInternalServiceProvider(serviceProvider).Options;
+
+            MyContext dbContext = new MyContext(options);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            return dbContext;
+        }
+
+        // Add inside BaseTest.cs
+        protected void CheckError<T>(AbstractValidator<T> validator, int ErrorCode, T vm)
+        {
+            var val = validator.Validate(vm);
+            Assert.False(val.IsValid);
+
+            if (!val.IsValid)
+            {
+                bool hasError = val.Errors.Any(a => a.ErrorCode.Equals(ErrorCode.ToString()));
+                Assert.True(hasError);
+            }
+        }
+    }
+}
