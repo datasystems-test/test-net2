163cdd8581c3816326469267ba6c46bbd0e4b6b3
datasystems-test <82836214+datasystems-test@users.noreply.github.com>
2021-05-26 15:43:13 -0500

    Update BaseTest.cs

--- CRUD-NETCore-TDD.Test/Tests/BaseTest.cs
+++ CRUD-NETCore-TDD.Test/Tests/BaseTest.cs
@@ -36,12 +36,12 @@ namespace CRUD_NETCore_TDD.Test.Tests
         protected void CheckError<T>(AbstractValidator<T> validator, int ErrorCode, T vm)
         {
             var val = validator.Validate(vm);
-            Assert.true(val.IsValid);
+            Assert.True(val.IsValid);
 
             if (!val.IsValid)
             {
                 bool hasError = val.Errors.Any(a => a.ErrorCode.Equals(ErrorCode.ToString()));
-                Assert.true(hasError);
+                Assert.True(hasError);
             }
         }
     }
